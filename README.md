6a9fc3d66b4366152349aa9c5fb0bb8f0b7d87ac
Evgeniy Khyst <evgeniy.khist@gmail.com>
2018-05-28 18:23:23 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # jenkins-example
+
+docker volume create jenkins-data
+docker volume create sonarqubedb-pgdata
+docker-compose up -d
+
+http://localhost:9000
+
+"Log in"
+
+"Generate a token"
+
+"Generate"
+
+Copy generated token 
+
+"Finish this tutorial"
+
+42711829e7b383b7f44e975f453765c36dcace57
+
+http://localhost:8080
+
+docker-compose exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
+
+"Install suggested plugins"
+
+"Create First Admin User"
+
+http://localhost:8080/blue
+
+"Create a new Pipeline"
+
+"GitHub"
+
+Follow the instructions to create personal access token...
+
+Select repository and "Create Pipeline"
+
+http://localhost:8080/swagger-ui.html
+http://localhost:8080/greeting
+http://localhost:8080/greeting?name=User
---
+++ sample-rest-service/src/main/java/com/example/samplerestservice/SwaggerConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Evgeniy Khyst
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.samplerestservice;
+
+import static springfox.documentation.builders.PathSelectors.any;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  /**
+   * Docket bean with Swagger configuration.
+   */
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(basePackage(SampleRestServiceApplication.class.getPackage().getName()))
+        .paths(any())
+        .build();
+  }
+}
