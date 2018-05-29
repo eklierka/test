7a1f01c2992e386a7a841b7e1fb3b984d8b8b72b
Evgeniy Khyst <evgeniy.khist@gmail.com>
2018-05-29 15:28:57 +0300

    Fix deploy.sh

--- sample-rest-service/deploy.sh
+++ sample-rest-service/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+echo "Removing running Docker container"
 docker rm -f ci-sample-rest-service || true
+
+echo "Removing cached Docker image"
 docker rmi localhost:5000/com.example/sample-rest-service
+
+echo "Pulling latest Docker image"
 docker pull localhost:5000/com.example/sample-rest-service
+
+echo "Running new Docker container"
 docker run -d --name ci-sample-rest-service -p 18080:8080 localhost:5000/com.example/sample-rest-service
+
+echo "Wating for application to start"
 grep -q "Started SampleRestServiceApplication" <(docker logs -f ci-sample-rest-service)
