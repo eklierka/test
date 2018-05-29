d78a26b9c7eb3a045789944c1ca3eb6bafd9c76f
Yevhen Khyst <yevhen.khyst@globallogic.com>
2018-05-29 18:25:42 +0300

    Various fixes

--- sample-rest-service/deploy.sh
+++ sample-rest-service/deploy.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-echo "Removing running Docker container"
-docker rm -f ci-sample-rest-service || true
+container_name=ci-sample-rest-service
+if [ -z "$1" ]
+then
+  image_tag="latest"
+else
+  image_tag="$1"
+fi
+image_name="localhost:5000/com.example/sample-rest-service:$image_tag"
 
-echo "Removing cached Docker image"
-docker rmi localhost:5000/com.example/sample-rest-service
+echo "Removing running Docker container $container_name"
+docker rm -f $container_name || true
 
-echo "Pulling latest Docker image"
-docker pull localhost:5000/com.example/sample-rest-service
+echo "Removing cached Docker image $image_name"
+docker rmi $image_name
 
-echo "Running new Docker container"
-docker run -d --name ci-sample-rest-service -p 18080:8080 localhost:5000/com.example/sample-rest-service
+echo "Pulling latest Docker image $image_name"
+docker pull $image_name
 
-echo "Wating for application to start"
-grep -q "Started SampleRestServiceApplication" <(docker logs -f ci-sample-rest-service)
+echo "Running new Docker container $container_name from image $image_name"
+docker run -d --name $container_name -p 18080:8080 $image_name
+
+echo "Waiting for the application to start"
+grep -q "Started SampleRestServiceApplication" <(docker logs -f container_name)
