500e093dbbc288029f56de4883572fa035330739
Evgeniy Khyst <evgeniy.khist@gmail.com>
2018-05-29 14:14:12 +0300

    Add deploy script

---
+++ sample-rest-service/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker rm -f ci-sample-rest-service || true
+docker pull localhost:5000/com.example/sample-rest-service
+docker run -d --name ci-sample-rest-service -p 18080:8080 localhost:5000/com.example/sample-rest-service
+timeout 180 grep -q "Started SampleRestServiceApplication" <(docker logs -f ci-sample-rest-service)
